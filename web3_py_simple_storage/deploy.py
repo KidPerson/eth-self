78df2fd254dbe16c83ff20143c07b42e1ed6ff92
kayblock <kelsye.dean@gmail.com>
2022-01-02 14:06:37 -0600

    added stuff (and works)

--- web3_py_simple_storage/deploy.py
+++ web3_py_simple_storage/deploy.py
@@ -1,4 +1,5 @@
-from solcx import compile_standard, install_solc  # <- import the install_solc method!
+# <- import the install_solc method!
+from solcx import compile_standard, install_solc
 import json
 from web3 import Web3
 import os
@@ -40,7 +41,7 @@ abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 # for connectiong to ganache
 w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
 chain_id = 1337
-my_address = "0x98998aA0D7D6096f7e604999D5808B89b0432430"
+my_address = os.getenv("PUBLIC_KEY")
 private_key = os.getenv("PRIVATE_KEY")
 
 # Create the contract in python
@@ -57,31 +58,37 @@ transaction = SimpleStorage.constructor().buildTransaction(
     }
 )
 # Signs transaction
-signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+signed_txn = w3.eth.account.sign_transaction(
+    transaction, private_key=private_key)
 # Send this signed transaction
 tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
-tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
 
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+# Working with the contract, you always need...
 # Contract Address
 # Contract ABI
 
 simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+
 # Call -> simulate making the call and getting a return value
 # Transact -> Actually make a state change
 
 # Initial value of favorite number
+# Creates transaction
+nonce = w3.eth.getTransactionCount(my_address)
 print(simple_storage.functions.retrieve().call())
-store_transaction = simple_storage.functions.store(15).buildTransaction
-(
-    {
-        "gasPrice": w3.eth.gas_price,
-        "chainId": chain_id,
-        "from": my_address,
-        "nonce": nonce + 1,
-    }
-)
+store_transaction = simple_storage.functions.store(15).buildTransaction({
+    "gasPrice": w3.eth.gas_price,
+    "chainId": chain_id,
+    "from": my_address,
+    "nonce": nonce,
+})
+# Signs transaction
 signed_store_txn = w3.eth.account.sign_transaction(
     store_transaction, private_key=private_key
 )
-send_store_txn = w3.eth.send_raw_transaction(signed_store_txn.rawTransaction)
-tx_receipt = w3.eth.wait_for_transaction_receipt(send_store_txn)
+# Sends transaction
+send_store_tx = w3.eth.send_raw_transaction(signed_store_txn.rawTransaction)
+
+# Waits for transaction
+tx_receipt = w3.eth.wait_for_transaction_receipt(send_store_tx)
